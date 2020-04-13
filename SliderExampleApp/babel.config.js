48b1d1311ba0c8877408b85fd7d7246c7c7b0ebf
Michael Blanchard <michael.james.blanchard@gmail.com>
2020-04-13 08:57:54 -0700

    WIP: expo app and workspace

--- SliderExampleApp/babel.config.js
+++ SliderExampleApp/babel.config.js
@@ -1,4 +1,6 @@
-/* @flow */
-module.exports = {
-    presets: ["module:metro-react-native-babel-preset"],
+module.exports = function(api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+  };
 };
