ae9412433f5f4b0527c5f0e5e908ae0180137d4b
Michael Blanchard <michael.james.blanchard@gmail.com>
2020-04-26 18:43:43 -0700

    delete merge duplicates

--- SliderExampleApp/babel.config.js
+++ SliderExampleApp/babel.config.js
@@ -1,6 +1,7 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+/* @noflow */
+module.exports = (api) => {
+    api.cache(true);
+    return {
+        presets: ["babel-preset-expo"],
+    };
 };
